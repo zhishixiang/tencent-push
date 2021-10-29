3e798be204de0edf3fc12575aed2d735a0e34b52
zhishixiang <1015256551@qq.com>
2021-10-29 19:13:16 +0800

    初始化

--- QQ-forward.py
+++ QQ-forward.py
@@ -1,12 +1,15 @@
-# bot.py
-
 from aiocqhttp import CQHttp, Event
 import httpx
 bot = CQHttp()
+#配置小米推送
+MiPush = True #是否开启小米推送，True为是，False为否
+alias = "ktIV8HjgEvOeKTj6" #请在括号内填入别名
 
-group_whitelist = {1077550597:'gkd'}    #群组白名单
-alias = 0000    #别名
+#配置FCM推送
+FCM = True #是否开启FCM推送，True为是，False为否
+key = "k-48dbfef698dd" #请在括号内填入API KEY
 
+group_whitelist = {1077550597:'gkd'} #群组白名单
 @bot.on_message('private')
 async def _(event: Event):
     msg = event['message']
@@ -24,8 +27,10 @@ async def _(event: Event):
         msg = "[合并转发]"
     nickName = event['sender']["nickname"]
     async with httpx.AsyncClient() as client:
-        await client.post("https://tdtt.top/send",data={'title':'来自QQ的私聊消息','content':'%s:%s'%(nickName,msg),'alias':alias})
-
+        if MiPush == True:
+            await client.post("https://tdtt.top/send",data={'title':'%s'%nickName,'content':'%s'%(msg),'alias':alias})
+        if FCM == True:
+            await client.post("http://xdroid.net/api/message",data={'t':'%s'%nickName,'c':'%s'%msg,'k':key})
 @bot.on_message('group')
 async def _(event: Event):
     groupId = event['group_id']
@@ -50,5 +55,8 @@ async def _(event: Event):
         elif nickName['card'] == "":
             nickName = nickName['nickname']
         async with httpx.AsyncClient() as client:
-            await client.post("https://tdtt.top/send",data={'title':'来自QQ的群聊消息:%s'%groupName,'content':'%s:%s'%(nickName,msg),'alias':alias})
+            if MiPush == True:
+                await client.post("https://tdtt.top/send",data={'title':'%s'%groupName,'content':'%s:%s'%(nickName,msg),'alias':alias})
+            if FCM == True:
+                await client.post("http://xdroid.net/api/message",data={'t':'%s'%groupName,'c':'%s:%s'%(nickName,msg),'k':key})
 bot.run(host='127.0.0.1', port=8080)
