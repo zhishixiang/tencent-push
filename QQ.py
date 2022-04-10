d44ed17fe4e2c26e4bd8d975a90ac359603c9c2c
zhishixiang <1015256551@qq.com>
2022-04-10 12:42:41 +0800

    添加加好友请求

--- QQ.py
+++ QQ.py
@@ -1,4 +1,5 @@
 from email import message
+from email.errors import FirstHeaderLineIsContinuationDefect
 from tokenize import group
 from flask import Flask,request,jsonify
 import json
@@ -73,12 +74,21 @@ async def recvMsg():
     if json_data["post_type"] == "meta_event":
         if json_data["meta_event_type"] == "heartbeat":
             print("接收心跳信号成功")
+    elif json_data["post_type"] == "request":
+        if json_data["request_type"] == "friend":
+            friendId = json_data["user_id"]
+            print("新的好友添加请求：%s"%friendId)
+            if MiPush == "True":
+                await httpx.AsyncClient().post("https://tdtt.top/send",data={'title':"新的好友添加请求",'content':'%s想要添加您为好友'%friendId,'alias':KEY})
+            elif FCM == "True":
+                await httpx.AsyncClient().post("https://wirepusher.com/send",data={'id':KEY,'title':"新的好友添加请求",'message':'%s想要添加您为好友'%friendId,'type':'FriendAdd'})            
+
     elif json_data["message_type"] == "private":
         nickName = json_data["sender"]["nickname"]
         msg = msgFormat(json_data["message"])
         print("来自%s的私聊消息:%s"%(nickName,msg))
         if MiPush == "True":
-            await httpx.AsyncClient().post("https://tdtt.top/send",data={'title':nickName,'content':'%s'%(msg),'alias':KEY})
+            await httpx.AsyncClient().post("https://tdtt.top/send",data={'title':nickName,'content':msg,'alias':KEY})
         elif FCM == "True":
             await httpx.AsyncClient().post("https://wirepusher.com/send",data={'id':KEY,'title':nickName,'message':msg,'type':'privateMsg'})
     elif json_data["message_type"] == "group":
